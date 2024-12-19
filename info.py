3a069b024de17c0c1fee605df828c1e3b484cb55
VishnuMB14 <vishnutuber@gmail.com>
2024-12-19 19:10:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,6 +39,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
+AUTH_CHANNELS = [-1002274354771, -1002363245479, "TGCinemaworld"]  # Replace with your channel IDs or usernames
 auth_channel = environ.get('AUTH_CHANNEL', '-1002274354771') #Channel / Group Id for force sub ( make sure bot is admin )
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
